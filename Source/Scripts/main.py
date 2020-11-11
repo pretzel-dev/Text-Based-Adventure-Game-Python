4a99e232c4531228e0a68a7e9c6510d018174a68
Pretzel <precelek.precelkowski@gmail.com>
2020-11-11 18:48:30 +0100

    Update of Menu
    - Added buttons
    - Buttons are working
        a) Exit button is 100% completed
        b) Play button clear the console
    - You can navigate thru menu with 'w', 's', 'up arror' and 'down arrow'
    - Centered a buttons

--- Source/Scripts/main.py
+++ Source/Scripts/main.py
@@ -1,23 +1,98 @@
-# Menu script
-
 from pyfiglet import Figlet
 import time
 import keyboard as input
+import os
+import shutil
 
 f = Figlet(font='ascii___')
 
-def DrawText(text):
-    if(str(text)!=""):
-        return(f.renderText(text))
-    else:
-        return("[OUR ERROR], please check size {size} <- that shouldn't be 0 and text {text} <- that should have something, can't be empty")
+currentoption = 0
 
+print('program started')
+
+
+def DrawText(text, center=True):
+    if(str(text)!= ''):
+        if(center is True):
+            print(*[x.center(shutil.get_terminal_size().columns) for x in f.renderText(text).split("\n")],sep="\n")
+        else:
+            print (f.renderText(text))
+    else:
+        print("[OUR ERROR], please check text {text} <- that should have something, can't be empty")
+    pass
+
+
+def NavigateMenu(key):
+    global currentoption
+    os.system('cls' if os.name == 'nt' else 'clear')
+    if(key == 'up'):
+        if currentoption == 0:
+            currentoption += 1
+        else:
+            currentoption = 0
+    else:
+        if currentoption == 1:
+            currentoption -= 1
+        else:
+            currentoption = 1
+        
+    DrawMenu()
+    pass
 
 
 def DrawMenu():
-    return(DrawText("play") + "\n" + DrawText("exit"))
+    global currentoption
+
+    print('\n\n\n\n\n')
+
+    if(currentoption == 0):
+        DrawText('-play-')
+        DrawText('exit')
+    else:
+        DrawText('play')
+        DrawText('-exit-')
+    pass
 
 
 
-print(DrawMenu())
-    
+def Play():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    pass
+
+def Exit():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    exit()
+    pass
+
+
+
+def ClickedButton():
+    global currentoption
+
+    if(currentoption == 0):
+        Play()
+    else:
+        Exit()
+    pass
+
+
+
+
+
+while True:
+    if input.is_pressed('w') or input.is_pressed('up_arrow'):
+        NavigateMenu('up')
+        time.sleep(1)
+    elif input.is_pressed('s') or input.is_pressed('down_arrow'):
+        NavigateMenu('down')
+        time.sleep(1)
+    elif input.is_pressed('esc'):
+        break
+    elif input.is_pressed('enter'):
+        ClickedButton()
+        time.sleep(1)
+
+
+
+        
+
